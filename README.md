7dc88b858d7ba93e539d2ac298c96f1033c8f50b
Mohammed Khallaf <mohammedkhallaf.dev@gmail.com>
2024-09-11 21:25:28 +0300

    finish the session

--- README.md
+++ README.md
@@ -26,8 +26,50 @@
 - 
 
 
+## Component from external library --> styling bootstrap component
+as={Ext.Co}
+
+## How to access local storage
+- window.localStorage => localStorage
+-  read <- `localStorage.getItem("key")`
+-  write <- `localStorage.setItem("key",value)`
+-  Note -> item @ save | read --> string
+-  JSON.strigify() <- from JS to string
+-  JSON.parse() -> from string to JS
+-  
+
+## lifecycle
+```jsx
+useEffect(()=>{ //==>
+
+},[])
+```
 
 ## Notes
 - How to check if bootstrap is installed
   - `package.json` -> look for `bootstrap` & `react-bootstrap`
 - all imports should be at the top of the file
+
+## Task 
+- Cart
+  - add to cart
+  - remove from cart
+- wishlist
+  - add and remove 
+  - protected
+- navbar -> cart & wishlist counters
+
+## Projects 
+- Electronics -> categories
+- MealMaster -> filters-> Recipe (plan) -> cart (list of products for recipes)
+- Book store -> categories -> cart , wishlist
+- Restaurant -> meals -> categories, offers -> cart
+- Clothes 
+- .. 
+- Cars
+- Admin Dashboard <-
+- TaskFlow -> to do list -> trello
+  - roles 
+  - -> admins assign tasks
+  - -> employees do and finish task
+- E-learning
--- src/App.js
+++ src/App.js
@@ -8,33 +8,85 @@ import Navbar from "react-bootstrap/Navbar";
 import "./App.css";
 import ProductCard from "./components/ProductCard";
 import ProductsGallery from "./pages/ProductsGallery";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import CartPage from "./pages/CartPage";
 import WishlistPage from "./pages/WishlistPage";
 import LoginPage from "./pages/LoginPage";
+import { useEffect, useState } from "react";
+import { Badge } from "react-bootstrap";
 
 function App() {
+  // conditional route -> protected --> condition
+  const [user, setUser] = useState();
+  // if user exists -> login page cannot open
+  // if user is not exists -> can open login
+  const [cartCounter, setCartCounter] = useState(0);
+  const location = useLocation();
+
+  const handleCounter = () => {
+    setCartCounter((prev) => {
+      localStorage.setItem("counter", prev + 1);
+      return prev + 1;
+    });
+  };
+  // listen to local storage
+  // save number
+  // update
+  // read number
+
+  useEffect(() => {
+    // initial render
+    const counter = localStorage.getItem("counter");
+    const counterAsnumber = JSON.parse(counter);
+
+    setCartCounter(counterAsnumber ?? 0);
+  }, []);
+
   return (
-    <BrowserRouter>
-      <div className="App">
+    <div className="App">
+      {!location.pathname.includes("login") && (
         <Navbar bg="dark" data-bs-theme="dark">
           <Container>
-            <Navbar.Brand href="#home">Amazon</Navbar.Brand>
+            <Navbar.Brand as={Link} to="/">
+              Amazon
+            </Navbar.Brand>
             <Nav className="ms-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Wishlist</Nav.Link>
-              <Nav.Link href="#pricing">Cart</Nav.Link>
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="wishlist">
+                Wishlist
+              </Nav.Link>
+              <Nav.Link as={Link} to="cart">
+                Cart
+                <Badge>{cartCounter}</Badge>
+              </Nav.Link>
+              {user ? null : (
+                <Nav.Link as={Link} to="login">
+                  Login
+                </Nav.Link>
+              )}
             </Nav>
           </Container>
         </Navbar>
-        <Routes>
-          <Route path="/" element={<ProductsGallery />} />
-          <Route path="cart" element={<CartPage />} />
-          <Route path="wishlist" element={<WishlistPage />} />
-          <Route path="login" element={<LoginPage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+      )}
+
+      <Button onClick={handleCounter}>Change the cart number</Button>
+
+      <Routes>
+        <Route path="/" element={<ProductsGallery />} />
+        <Route path="cart" element={<CartPage />} />
+        <Route path="wishlist" element={<WishlistPage />} />
+        {!user && <Route path="login" element={<LoginPage />} />}
+        <Route path="*" element={<Container>Not Found</Container>} />
+      </Routes>
+    </div>
   );
 }
 // .Provider
